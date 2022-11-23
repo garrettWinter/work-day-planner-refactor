fe2476de4de7658ad8f54e5f5bf25178f3ae318e
Garrett Winter <Garrett_Winter@hotmail.com>
2022-11-23 00:44:09 -0600

    The Red, Green, Gray background color logic is now functional.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,69 @@
 var todayDay = moment().format('dddd');
 var dateTime = moment().format('MMMM Do YYYY');
-console.log(todayDay);
-console.log(dateTime);
-
 var dayDisplay = document.querySelector("#day");
-var dateTimeDisplay  = document.querySelector("#date");
+var dateTimeDisplay = document.querySelector("#date");
 var saveButton = document.querySelector('.saveBox');
-var storeageText= document.querySelector("#storeageMessage");
+var storeageText = document.querySelector("#storeageMessage");
 var commentsField = document.querySelector('.comments');
+var logicTestVar = document.querySelector('.testingDiv');
+// var currentHour = moment().format('HH');
+var currentHour = 10;
+console.log(currentHour)
+var storageArray = [
+    [9, ""],
+    [10, ""],
+    [11, ""],
+    [12, ""],
+    [13, ""]
+];
+
+
+/* Vars created t0 clear errors */
+
+var commentBlock = document.querySelector('.commentBlock');
 
 dayDisplay.textContent = todayDay;
 dateTimeDisplay.textContent = dateTime;
 
 /* This runs on page load to display any comments stored in local storage */
-function init (){
+function init() {
     console.log("initFunction has run")
     commentsField.value = JSON.parse(localStorage.getItem("testValue"));
+    logicTestVar.textContent = "Connection Test";
+    var formulaTest = 0;
+    formulaTest = storageArray[0][0] - currentHour;
+    console.log(formulaTest);
+    if (formulaTest < 0) {
+        // commentBlock.style.backgroundColor = "#d3d3d3";
+        commentBlock.classList.add("past")
+    }
+    else if (formulaTest > 0) {
+        // commentBlock.style.backgroundColor = "#77dd77";
+        commentBlock.classList.add("future")
+    }
+    else {
+        // commentBlock.style.backgroundColor = "#ff6961";
+        commentBlock.classList.add("present")
+    };
 }
 
+function timeLogic (){
+
+};
+
 /* This is save feature to store comments into local storage for future referance */
-function commentStorage () {
+function commentStorage() {
     console.log("noteStorage Function has been triggered");
     storeageText.textContent = "Appointment has been added to local storage.";
     comments = commentsField.value;
     console.log(comments);
-    localStorage.setItem("testValue",JSON.stringify(commentsField.value));
-
+    localStorage.setItem("testValue", JSON.stringify(commentsField.value));
 }
-
-
-
-
 
 
 /* Event Listeners */
 saveButton.addEventListener("click", commentStorage);
- init()
+init()
 
 /* 
 
